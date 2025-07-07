119e4379c2ca3365b366312bac09342a3e687eb1
salstq <83420789+salstq@users.noreply.github.com>
2025-07-07 13:00:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 import streamlit as st
 import numpy as np
-import tensorflow as tf
+import onnxruntime as ort
 import tensorflow_hub as hub
 import librosa
-from tensorflow.keras.models import load_model
 from sklearn.preprocessing import LabelEncoder
 
-# === Load model dan encoder ===
+# === Judul app ===
 st.title("Deteksi Jenis Tangisan Bayi 👶🔊")
 
+# === Load model dan encoder ===
 @st.cache_resource
 def load_yamnet():
     return hub.load("https://tfhub.dev/google/yamnet/1")
 
 @st.cache_resource
-def load_classifier_model():
-    return load_model("best_model.h5")
+def load_onnx_model():
+    return ort.InferenceSession("best_model.onnx")
 
 @st.cache_resource
 def load_label_encoder():
@@ -24,23 +24,21 @@ def load_label_encoder():
     return encoder
 
 yamnet_model = load_yamnet()
-best_model = load_classifier_model()
+onnx_model = load_onnx_model()
 encoder = load_label_encoder()
 
 # === Fungsi prediksi ===
 def extract_mean_embedding(file):
-    # Load audio dan resample ke 16kHz
     wav, sr = librosa.load(file, sr=16000)
-    wav_tensor = tf.convert_to_tensor(wav, dtype=tf.float32)
-    
-    # Dapatkan embedding dari YAMNet
+    wav_tensor = tf.convert_to_tensor(wav, dtype=tf.float32)  # tetap pakai tf di sini
     _, embeddings, _ = yamnet_model(wav_tensor)
-    mean_embedding = tf.reduce_mean(embeddings, axis=0)
-    return tf.expand_dims(mean_embedding, axis=0)  # Tambah batch dimensi
+    mean_embedding = tf.reduce_mean(embeddings, axis=0).numpy()
+    return np.expand_dims(mean_embedding, axis=0)
 
 def predict_audio_class(audio_file):
-    embedding = extract_mean_embedding(audio_file)
-    prediction = best_model.predict(embedding)
+    embedding = extract_mean_embedding(audio_file).astype(np.float32)
+    inputs = {onnx_model.get_inputs()[0].name: embedding}
+    prediction = onnx_model.run(None, inputs)[0]
     predicted_index = np.argmax(prediction)
     predicted_label = encoder.inverse_transform([predicted_index])[0]
     confidence = np.max(prediction)
@@ -51,7 +49,6 @@ uploaded_file = st.file_uploader("Upload file audio .wav", type=["wav"])
 
 if uploaded_file is not None:
     st.audio(uploaded_file, format="audio/wav")
-
     with st.spinner("Mendeteksi..."):
         label, confidence = predict_audio_class(uploaded_file)
         st.success(f"✅ Prediksi: **{label}**")
